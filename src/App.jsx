63bf42d131a5fe234a7b7bdf0646f9c91a949056
AndreNovaisBrito <andrenoz@gmail.com>
2023-02-10 12:35:38 -0300

    all objectives

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [quote, setQuote] = useState('Nice quote 1');
+  const [author, setAuthor] = useState('Jaesnan');
+  function handleClick(){
+    setQuote('Second Quote')
+    setAuthor('Félix')
+  }
   return (
     <div className="App">
-      <div>Hello
-      </div>
+      <wrapper id='quote-box'>
+
+        <h1 id='text'>{quote}</h1>
+        <h2 id='author'>{author}</h2>
+
+        <button id='new-quote' onClick={handleClick}>
+          New Quote
+        </button>
+        <button >
+          <a id='tweet-quote' href='twitter.com/intent/tweet' target="_top">Tweet</a>
+        </button>
+      </wrapper>
     </div>
   )
 }
