6147ce281ffd01ce3adef7bc67093cdce2a3b749
AndreNovaisBrito <andrenoz@gmail.com>
2023-02-13 10:57:22 -0300

    Add twitter Link

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,35 @@ const quoteBank = [
   {quote: "Believe you can and you're halfway there.", author: "Theodore Roosevelt"},
   {quote: "Your time is limited, don't waste it living someone else's life.", author: "Steve Jobs"},
   {quote: "Don't watch the clock; do what it does. Keep going.", author: "Sam Levenson"},
+  {quote: "Successful and unsuccessful people do not vary greatly in their abilities. They vary in their desires to reach their potential.", author: "John Maxwell"},
+  {quote: "Successful people do what unsuccessful people are not willing to do.", author: "Jim Rohn"},
+  {quote: "You are never too old to set another goal or to dream a new dream.", author: "C.S. Lewis"},
+  {quote: "Success is not how high you have climbed, but how you make a positive difference to the world.", author: "Roy T. Bennett"},
+  {quote: "Success is not the key to happiness. Happiness is the key to success.", author: "Albert Schweitzer"},
+  {quote: "Success is not a destination, it's a journey.", author: "Zig Ziglar"},
+  {quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.", author: "Winston Churchill"},
+  {quote: "A goal is not always meant to be reached, it often serves simply as something to aim at.", author: "Bruce Lee"},
+  {quote: "The only way to do great work is to love what you do.", author: "Steve Jobs"},
+  {quote: "Success is a journey, not a destination. The doing is often more important than the outcome.", author: "Arthur Ashe"},
+  {quote: "Success is not the absence of failure; it's the persistence through failure.", author: "A. J. Johns"},
+  {quote: "Success is not how much money you make, but how much you keep, how hard it works for you, and how many generations you keep it for.", author: "Robert Kiyosaki"},
+  {quote: "Successful people are always looking for opportunities to help others. Unsuccessful people are always asking, 'What's in it for me?'", author: "Brian Tracy"},
+  {quote: "Success is stumbling from failure to failure with no loss of enthusiasm.", author: "Winston Churchill"}
 ]
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * 9);
+function getRandomNumber(array) {
+  return Math.floor(Math.random() * array.length);
 }
 
 function App() {
-  let randomNumber = getRandomNumber();
-  console.log(randomNumber);
+  let randomNumber = getRandomNumber(quoteBank);
   const [quote, setQuote] = useState(quoteBank[randomNumber].quote);
   const [author, setAuthor] = useState(quoteBank[randomNumber].author);
 
+  let twitterLink= `https://twitter.com/intent/tweet?text="${quote}"- ${author}`
+
   function handleClick(){
-    let randomNumber2  = getRandomNumber();
+    let randomNumber2  = getRandomNumber(quoteBank);
     setQuote(quoteBank[randomNumber2].quote)
     setAuthor(quoteBank[randomNumber2].author)
   }
@@ -41,13 +56,14 @@ function App() {
           <button id='new-quote' onClick={handleClick}>
             New Quote
           </button>
-          <button >
-            <a id='tweet-quote' href='twitter.com/intent/tweet' target="_blank">Tweet</a>
+          <button>
+            <a id='tweet-quote' href={twitterLink} target="_blank">Tweet</a>
           </button>
         </div>
       </div>
     </div>
   )
 }
+
 
 export default App
